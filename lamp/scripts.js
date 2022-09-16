71d613b2d032aba6053a35be74610d2b6f4008c5
oleg <98418088+etoOleh@users.noreply.github.com>
2022-09-16 12:30:29 +0300

    Сайт с лампочкой

---
+++ lamp/scripts.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("btn");
+let light = document.getElementById("light");
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on")
+}
